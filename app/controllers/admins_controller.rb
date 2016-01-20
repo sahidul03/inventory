0b714704d573daa168982e7ad9e0e1584938acfa
sahidulislam <sahidul.islam@rootnext.com>
2016-01-20 20:26:51 +0600

    admin creation continue....

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,26 @@
+class AdminsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user=User.new(params_admin)
+      if @user.save
+        flash[:notice] = "Admin created successfully."
+        redirect_to admins_path
+      else
+        render 'new'
+      end
+  end
+
+  protected
+
+  def params_admin
+    params.require(:user).permit(:email,:password, :password_confirmation)
+  end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :user_type) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :user_type) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :user_type) }
   end
 
 end
