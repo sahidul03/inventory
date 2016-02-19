58d16d638a540d2cbfc3a3fb4087a8981948e7c2
sahidulislam <sahidul.islam@rootnext.com>
2016-02-19 12:39:45 +0600

    turbo link true set

--- app/models/food_category.rb
+++ app/models/food_category.rb
@@ -1,7 +1,9 @@
 class FoodCategory < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :discount, :numericality => true
+  validates_numericality_of :discount, :greater_than_or_equal_to => 0
   mount_uploader :photo, FoodCategoryPhotoUploader
+
   belongs_to :user
   has_many :food_sub_categories
 end
--- app/models/food_sub_category.rb
+++ app/models/food_sub_category.rb
@@ -1,7 +1,9 @@
 class FoodSubCategory < ActiveRecord::Base
   validates :name, :food_category_id, :price, presence: true
-  validates :discount_tk, :price, :numericality => true
+  # validates :discount_tk, :price, :numericality => true
+  validates_numericality_of :discount_tk, :price, :greater_than_or_equal_to => 0
   mount_uploader :photo, FoodCategoryPhotoUploader
+
   belongs_to :user
   belongs_to :food_category
 end
