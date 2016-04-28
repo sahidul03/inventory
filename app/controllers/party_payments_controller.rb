747776950f48292abc6ab623b2343495f94275eb
sahidulislam <sahidul.islam@rootnext.com>
2016-04-28 17:32:48 +0600

    party payment reports

--- app/controllers/party_payments_controller.rb
+++ app/controllers/party_payments_controller.rb
@@ -23,7 +23,7 @@ class PartyPaymentsController < ApplicationController
     @date = Date.today
     @month = Month.find(@date.strftime("%-m").to_i) rescue nil
     @year= Year.find_by_name(@date.strftime("%Y").to_s) rescue nil
-    @reports = BankBalanceOut.where(:created_at => Date.today.beginning_of_month..Date.today.end_of_month)
+    @reports = PartyPayment.where(:created_at => Date.today.beginning_of_month..Date.today.end_of_month)
 
     respond_to do |format|
       format.html
@@ -38,19 +38,23 @@ class PartyPaymentsController < ApplicationController
     @month = Month.find(params[:month_id].to_i) rescue nil
     @year= Year.find_by_name(params[:year_id].to_s) rescue nil
     @bank_account = BankAccount.find(params[:bank_account_id]) rescue nil
+    @party = Party.find(params[:party_id]) rescue nil
 
     if params[:month_id] !='' && params[:year_id] != ''
       @year_month= DateTime.new( params[:year_id].to_i ,params[:month_id].to_i)
-      @reports = BankBalanceOut.where(:created_at => @year_month.beginning_of_month..@year_month.end_of_month)
+      @reports = PartyPayment.where(:created_at => @year_month.beginning_of_month..@year_month.end_of_month)
     elsif params[:year_id] != ''
       @year_month= DateTime.new( params[:year_id].to_i)
-      @reports = BankBalanceOut.where(:created_at => @year_month.beginning_of_year..@year_month.end_of_year)
+      @reports = PartyPayment.where(:created_at => @year_month.beginning_of_year..@year_month.end_of_year)
     else
-      @reports = BankBalanceOut.all
+      @reports = PartyPayment.all
     end
 
     if @bank_account
       @reports = @reports.where(:bank_account_id => @bank_account.id)
+    end
+    if @party
+      @reports = @reports.where(:party_id => @party.id)
     end
   end
 
@@ -58,19 +62,23 @@ class PartyPaymentsController < ApplicationController
     @month = Month.find(params[:month_id].to_i) rescue nil
     @year= Year.find_by_name(params[:year_id].to_s) rescue nil
     @bank_account = BankAccount.find(params[:bank_account_id]) rescue nil
+    @party = Party.find(params[:party_id]) rescue nil
 
     if params[:month_id] !='' && params[:year_id] != ''
       @year_month= DateTime.new( params[:year_id].to_i ,params[:month_id].to_i)
-      @reports = BankBalanceOut.where(:created_at => @year_month.beginning_of_month..@year_month.end_of_month)
+      @reports = PartyPayment.where(:created_at => @year_month.beginning_of_month..@year_month.end_of_month)
     elsif params[:year_id] != ''
       @year_month= DateTime.new( params[:year_id].to_i)
-      @reports = BankBalanceOut.where(:created_at => @year_month.beginning_of_year..@year_month.end_of_year)
+      @reports = PartyPayment.where(:created_at => @year_month.beginning_of_year..@year_month.end_of_year)
     else
-      @reports = BankBalanceOut.all
+      @reports = PartyPayment.all
     end
 
     if @bank_account
       @reports = @reports.where(:bank_account_id => @bank_account.id)
+    end
+    if @party
+      @reports = @reports.where(:party_id => @party.id)
     end
 
     respond_to do |format|
@@ -86,7 +94,7 @@ class PartyPaymentsController < ApplicationController
     @date = Date.today
     @start_date = @date.beginning_of_month
     @end_date = @date.end_of_month
-    @reports = BankBalanceOut.where(:created_at => Date.today.beginning_of_month..Date.today.end_of_month)
+    @reports = PartyPayment.where(:created_at => Date.today.beginning_of_month..Date.today.end_of_month)
 
     respond_to do |format|
       format.html
@@ -98,22 +106,30 @@ class PartyPaymentsController < ApplicationController
   end
 
   def date_to_date_report_search
+    @party = Party.find(params[:party_id]) rescue nil
     @bank_account = BankAccount.find(params[:bank_account_id]) rescue nil
     @start_date = Date.parse(params[:start_date])
     @end_date = Date.parse(params[:end_date])
-    @reports = BankBalanceOut.where(:created_at => @start_date.beginning_of_day..@end_date.end_of_day)
+    @reports = PartyPayment.where(:created_at => @start_date.beginning_of_day..@end_date.end_of_day)
     if @bank_account
       @reports = @reports.where(:bank_account_id => @bank_account.id)
+    end
+    if @party
+      @reports = @reports.where(:party_id => @party.id)
     end
   end
 
   def date_to_date_report_download
+    @party = Party.find(params[:party_id]) rescue nil
     @bank_account = BankAccount.find(params[:bank_account_id]) rescue nil
     @start_date = Date.parse(params[:start_date])
     @end_date = Date.parse(params[:end_date])
-    @reports = BankBalanceOut.where(:created_at => @start_date.beginning_of_day..@end_date.end_of_day)
+    @reports = PartyPayment.where(:created_at => @start_date.beginning_of_day..@end_date.end_of_day)
     if @bank_account
       @reports = @reports.where(:bank_account_id => @bank_account.id)
+    end
+    if @party
+      @reports = @reports.where(:party_id => @party.id)
     end
 
     respond_to do |format|
